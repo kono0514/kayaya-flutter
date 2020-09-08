047aaf5a23de0aaf3afcaf29e4e25fdb2660682c
Uuganbaatar <38470934+kono0514@users.noreply.github.com>
2020-09-08 22:26:19 +0800

    Firebase notification initial integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:kayaya_flutter/cubit/browse_filter_cubit.dart';
 import 'package:kayaya_flutter/cubit/genre_list_cubit.dart';
@@ -13,6 +15,9 @@ import 'package:kayaya_flutter/repository.dart';
 import 'package:kayaya_flutter/simple_bloc_observer.dart';
 import 'package:kayaya_flutter/theme_data.dart';
 import 'package:kayaya_flutter/widgets/navigation_bar/navigation_tab.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -42,10 +47,54 @@ void main() async {
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   final Locale locale;
 
   const MyApp({Key key, this.locale}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _createNotificationChannel();
+
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        // _showItemDialog(message);
+      },
+      onBackgroundMessage: myBackgroundMessageHandler,
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        // _navigateToItemDetail(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+        // _navigateToItemDetail(message);
+      },
+    );
+    _firebaseMessaging.getToken().then((value) => print(value));
+  }
+
+  Future<void> _createNotificationChannel() async {
+    var androidNotificationChannel = AndroidNotificationChannel(
+      '1', // channel ID
+      'Subscriptions', // channel name
+      "New episode on a series you've subsribed", //channel description
+      importance: Importance.High,
+    );
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(androidNotificationChannel);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,7 +111,7 @@ class MyApp extends StatelessWidget {
             GlobalWidgetsLocalizations.delegate,
             GlobalCupertinoLocalizations.delegate,
           ],
-          locale: locale,
+          locale: widget.locale,
           supportedLocales: S.delegate.supportedLocales,
           home: RootScreen(),
           onGenerateRoute: Routes.materialRoutes,
@@ -77,4 +126,19 @@ class RootScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(body: NavigationTab());
   }
+}
+
+Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) async {
+  print('Received message in background');
+  if (message.containsKey('data')) {
+    // Handle data message
+    final dynamic data = message['data'];
+  }
+
+  if (message.containsKey('notification')) {
+    // Handle notification message
+    final dynamic notification = message['notification'];
+  }
+
+  // Or do other work.
 }
